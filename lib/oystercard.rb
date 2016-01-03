091a548982d0e20e0907b531ced11cb24a232dea
Andrew <andy_htun@hotmail.com>
2016-01-03 15:13:27 +0000

    maximum balance function added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,16 @@
 class Oystercard
-end 
+
+  attr_reader :balance
+
+  MAXIMUM_BALANCE = 90
+
+  def initialize
+    @balance = 0
+  end
+
+  def top_up(amount)
+    fail 'Maximum balance allowed is £90' if @balance + amount > 90
+    @balance = @balance + amount
+  end
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,18 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+  it 'has a balance of zero when initialized' do
+    oystercard = Oystercard.new
+    expect(oystercard.balance).to eq 0
+  end
+
+  it 'tops up the balance of the card' do
+    oystercard = Oystercard.new
+    expect{ oystercard.top_up(10) }.to change{ oystercard.balance }.by 10
+  end
+
+  it 'raises an error if the user exceeds the maximum amount' do
+    oystercard = Oystercard.new
+    expect{oystercard.top_up(100)}.to raise_error "Maximum balance allowed is £90"
+  end
 end
