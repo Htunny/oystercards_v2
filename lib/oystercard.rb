378c5a6af4d5750ffc6635b13d17075fc59efca8
Andrew <andy_htun@hotmail.com>
2016-01-03 15:51:33 +0000

    touch in/out function added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,16 +1,37 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :in_journey
 
   MAXIMUM_BALANCE = 90
+  FARE = 2.50
 
   def initialize
     @balance = 0
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
   end
 
   def top_up(amount)
-    fail 'Maximum balance allowed is £90' if @balance + amount > 90
+    fail "Maximum balance of £#{MAXIMUM_BALANCE} exceeded" if @balance + amount > MAXIMUM_BALANCE
     @balance = @balance + amount
   end
+
+  def deduct(amount)
+    @balance = @balance - amount
+  end
+
+  def touch_in(entry_station)
+    @balance = @balance - FARE
+    @in_journey = true
+  end
+
+  def touch_out(exit_station)
+    @balance = @balance - FARE
+    @in_journey = false
+  end
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,18 +1,60 @@
 require 'oystercard'
 
 describe Oystercard do
+  maximum_balance = Oystercard::MAXIMUM_BALANCE
+  fare = Oystercard::FARE
+  subject(:oystercard) { described_class.new }
+
+describe '#new card' do
   it 'has a balance of zero when initialized' do
-    oystercard = Oystercard.new
     expect(oystercard.balance).to eq 0
   end
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+end
 
+describe '#top up balance' do
   it 'tops up the balance of the card' do
-    oystercard = Oystercard.new
     expect{ oystercard.top_up(10) }.to change{ oystercard.balance }.by 10
   end
 
   it 'raises an error if the user exceeds the maximum amount' do
-    oystercard = Oystercard.new
-    expect{oystercard.top_up(100)}.to raise_error "Maximum balance allowed is £90"
+    expect{ oystercard.top_up(100) }.to raise_error "Maximum balance of £#{maximum_balance} exceeded"
   end
+end
+
+describe '#touch in' do
+  it 'deducts a fare from the card' do
+    oystercard.top_up(maximum_balance)
+    expect{ oystercard.deduct(fare) }.to change{ oystercard.balance }.by -fare
+  end
+
+  it 'allows a user to touch in' do
+    oystercard.top_up(maximum_balance)
+    expect{ oystercard.touch_in("Aldgate") }.to change{ oystercard.balance }.by -fare
+  end
+
+  it 'changes in_journey? to true when touched in' do
+    oystercard.top_up(maximum_balance)
+    oystercard.touch_in("Aldgate")
+    expect(oystercard.in_journey?).to eq true
+  end
+  it { is_expected.to respond_to(:deduct).with(1).argument }
+  it { is_expected.to respond_to(:touch_in).with(1).argument }
+end
+
+describe '#touch out' do
+  it 'allows a user to touch out' do
+    oystercard.top_up(maximum_balance)
+    oystercard.touch_in("Aldgate")
+    expect{ oystercard.touch_out("Waterloo") }.to change{ oystercard.balance }.by -fare
+  end
+
+  it 'changes in_journey? to false when touched out' do
+    oystercard.top_up(maximum_balance)
+    oystercard.touch_in("Aldgate")
+    expect{ oystercard.touch_out("Waterloo") }.to change{ oystercard.in_journey?}.to false
+  end
+    it { is_expected.to respond_to(:touch_out).with(1).argument }
+end
+
 end
